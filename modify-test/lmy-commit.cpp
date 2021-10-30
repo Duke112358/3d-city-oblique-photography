e147a52bc07aac37a3b7ab245bd911158a29659b
naichamilktea <78580403+naichamilktea@users.noreply.github.com>
2021-10-30 12:13:27 +0800

    Add files via upload

---
+++ modify-test/lmy-commit.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define rep(i,a,n) for(int i=a;i<n;++i)
+#define pb push_back
+typedef long long ll;
+#define sx(x) ((ll)(x).size())
+typedef vector<ll> vi;
+const ll mod=998244353;
+/*
+					·ð×æ±£ÓÓ ÓÀÎÞbug 
+                       _oo0oo_
+                      o8888888o
+                      88" . "88
+                      (| -_- |)
+                      0\  =  /0
+                    ___/`---'\___
+                  .' \\|     |// '.
+                 / \\|||  :  |||// \
+                / _||||| -:- |||||- \
+               |   | \\\  -  /// |   |
+               | \_|  ''\---/''  |_/ |
+               \  .-\__  '-'  ___/-. /
+             ___'. .'  /--.--\  `. .'___
+          ."" '<  `.___\_<|>_/___.' >' "".
+         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+         \  \ `_.   \_ __\ /__ _/   .-` /  /
+     =====`-.____`.___ \_____/___.-`___.-'=====
+                       `=---='
+
+*/
+
+
+int main(){
+	cout<<"hello world\n";
+	return 0;
+}
+
